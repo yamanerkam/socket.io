a9abf04dddc95e26ab1de002a049af98ac41dfd8
Kam <erkamyaman35@gmail.com>
2024-05-23 23:19:02 +0300

    user-id

--- api/server.js
+++ api/server.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const { Server } = require('socket.io')
-const cors = require('cors')
-const port = 3000
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-// research midleweare 
-// research app.use
-app.use(cors())
+app.use(cors());
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
 
-    }
-})
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
 
-io.on('connection', (socket) => {
-    socket.emit('connect', { message: 'a new client connected' })
-})
 
-server.listen(port, () => {
-    console.log(`listening on ${port}`)
-})
+});
+
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING");
+});
