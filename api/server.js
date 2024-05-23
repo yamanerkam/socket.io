94016bf04cddb9d7e022684c0669ff90449bf87b
Kam <erkamyaman35@gmail.com>
2024-05-23 20:43:50 +0300

    socket.io-start

--- api/server.js
+++ api/server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
-
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
 const port = 3000
 
-const io = require('socket.io')(port + 1)
-io.on('connection', socket => {
-    console.log(socket.id)
-})
 // research midleweare 
 // research app.use
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('hi there!')
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+
+    }
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`listening on ${port}`)
 })
